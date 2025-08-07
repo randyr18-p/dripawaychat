f479274be7ad1ce8fd8c41de52314264b3a98217
randyr18-p <randyrpaulp@gmail.com>
2025-08-07 12:32:27 -0500

    Add Procfile and configure CORS middleware in main.py

--- app/main.py
+++ app/main.py
@@ -9,7 +9,27 @@ import os
 
 load_dotenv()
 
+
+
+
 app = FastAPI()
+
+
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 # --- Configuración de JWT ---
 SECRET_KEY = os.getenv("JWT_SECRET_KEY")
